f1a05e2799b4cc4bd2242f3e1f43746afe4f3cdb
Jared Bannan <jared.bannan@spatialfront.com>
2024-07-15 10:15:37 -0400

    add upload, pdf-lib

---
+++ src/components/FileUpload/FileUpload.tsx
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+
+export const FileUpload = () => {
+  return (
+    <div className="file-up-area">
+      <input type="file" accept=".pdf" ></input>
+    </div>
+  );
+};
