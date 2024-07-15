8500d5a30e30e04a49418b3421a87c5c9d4f4482
Jared Bannan <jared.bannan@spatialfront.com>
2024-07-15 14:55:24 -0400

    make it run

--- src/components/PDFReader/PDFReaderVM.ts
+++ src/components/PDFReader/PDFReaderVM.ts
@@ -1,70 +1,71 @@
 import { makeAutoObservable } from "mobx";
-import { PDFDocument, PDFField } from 'pdf-lib'
+import { PDFDocument, PDFField } from "pdf-lib";
 
 // PDF manipulation here
 export class PDFReaderVM {
+  pdfFile: File | undefined = undefined;
+  textFields: ITextFields[] = [];
 
-    pdfFile: File | undefined = undefined;
-    textFields: ITextFields[] = [];
+  constructor() {
+    makeAutoObservable(this, {}, { autoBind: true });
+  }
 
+  fileSubmitCallback(file: File) {
+    console.log("file Submit Callback with file: ", file.name);
+    this.pdfFile = file;
+  }
 
-    constructor() {
-        makeAutoObservable(this, {}, { autoBind: true })
+  async handlePDFFormFields() {
+    if (this.pdfFile === undefined) {
+      return;
     }
+    const pdfDoc = await PDFDocument.load(await this.pdfFile.arrayBuffer());
+    const form = pdfDoc.getForm();
+    const fields = form.getFields();
+    fields.forEach((field) => {
+      const type = field.constructor.name;
+      const name = field.getName();
+      let value: any = undefined;
+      switch (type) {
+        case "PDFCheckBox":
+          value = this.valueFromCheckbox(field);
+          break;
+        case "PDFTextField":
+          value = this.valueFromText(field);
+          break;
+        case "PDFDropdown":
+          value = this.valueFromDropdown(field);
+          break;
+      }
+      console.log(`${type}: ${name}`);
+    });
+    return;
+  }
 
-    fileSubmitCallback(file: File) {
-        console.log('file Submit Callback with file: ', file.name);
-        this.pdfFile = file;
-    }
+  valueFromCheckbox(field: PDFField) {
+    // const value = field.getText()
+    return false;
+  }
+  valueFromText(field: PDFField) {
+    // field =
+    return false;
+  }
+  valueFromDropdown(field: PDFField) {
+    // field =
+    return false;
+  }
 
-    async handlePDFFormFields() {
-        if (this.pdfFile === undefined) {
-            return;
-        }
-        const pdfDoc = await PDFDocument.load(await this.pdfFile.arrayBuffer());
-        const form = pdfDoc.getForm();
-        const fields = form.getFields();
-        fields.forEach(field => {
-            const type = field.constructor.name;
-            const name = field.getName();
-            let value = undefined;
-            switch(type) {
-                case 'PDFCheckBox':
-                    value = this.valueFromCheckbox(field);
-                    break;
-                case 'PDFTextField':
-                    value = this.valueFromText(field)
-                    break;
-                case 'PDFDropdown':
-                    value = this.valueFromDropdown(field);
-                    break;
-            }
-            console.log(`${type}: ${name}`);
-          })
-        return;
-    }
-
-    valueFromCheckbox(field: PDFField) {
-        // const value = field.getText()
-    }
-    valueFromText(field: PDFField) {
-        // field = 
-    }
-    valueFromDropdown(field: PDFField) {
-        // field = 
-    }
-
-    // Quiz 1
-    // async handleFormlessPDF() {
-    //     if (this.pdfFile === undefined) {
-    //         return;
-    //     }
-    //     const pdfDoc = await PDFDocument.load(await this.pdfFile.arrayBuffer());
-    //     const form = pdfDoc.getForm();
-    // }
+  // Quiz 1
+  // async handleFormlessPDF() {
+  //     if (this.pdfFile === undefined) {
+  //         return;
+  //     }
+  //     const pdfDoc = await PDFDocument.load(await this.pdfFile.arrayBuffer());
+  //     const form = pdfDoc.getForm();
+  // }
 }
 
 interface ITextFields {
-    name: 'string';
-    value: any;
+  name: "string";
+  value: any;
 }
